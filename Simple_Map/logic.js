404c7daff1c43b0c4725251a1d90fe9ed2ad8a64
tylerwe19 <tylerwe19@gmail.com>
2023-02-09 22:56:33 -0600

    Uploading Multi Point Map

--- Simple_Map/logic.js
+++ Simple_Map/logic.js
@@ -21,13 +21,28 @@ let map = L.map('map').setView([40.7, -94.5], 4);
 //     accessToken: API_KEY
 //});
 
-//  Add a marker to the map for Los Angeles, California.
-let marker = L.marker([34.0522, -118.2437]).addTo(map);
-L.circleMarker([34.0522,-118.2437],{
-    radius: 300,
-    color: "black",
-    fillColor: '#ffffa1'
-}).addTo(map);
+//  Add a single point marker to the map for Los Angeles, California.
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+// L.circleMarker([34.0522,-118.2437],{
+//     radius: 300,
+//     color: "black",
+//     fillColor: '#ffffa1'
+// }).addTo(map);
+
+//Mapping Multiple Points
+//Get data from cities.js
+let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population/100000
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map)
+   });
+
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
