85a8d3f42706cbba9fd001131d2da8276d77390c
tylerwe19 <tylerwe19@gmail.com>
2023-02-09 18:42:25 -0600

    Uploading files

--- Simple_Map/logic.js
+++ Simple_Map/logic.js
@@ -21,6 +21,14 @@ let map = L.map('map').setView([40.7, -94.5], 4);
 //     accessToken: API_KEY
 //});
 
+//  Add a marker to the map for Los Angeles, California.
+let marker = L.marker([34.0522, -118.2437]).addTo(map);
+L.circleMarker([34.0522,-118.2437],{
+    radius: 300,
+    color: "black",
+    fillColor: '#ffffa1'
+}).addTo(map);
+
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
 attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
